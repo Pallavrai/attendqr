92065d1f2cf21cc0fb53e853f679673d497b19ef
Pallav Rai <pallavrai8953@gmail.com>
2021-11-03 00:49:44 +0000

    edited setting file

--- attendsr/settings.py
+++ attendsr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!&97pv=u04brj#e$(dlqcbaq*z-8=_3$u6fjnv3q-$zavre(2$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','attendsr.herokuapp.com']
 
 
 # Application definition
